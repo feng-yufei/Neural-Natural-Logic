15b39cc076388aa28fd40b8ef3034a22cdd20595
Yufei Feng <zjufengyufei@gmail.com>
2021-06-23 20:25:19 -0400

    Add files via upload

---
+++ utils/dataset/classification.py
@@ -0,0 +1,56 @@
+""" Dataset container for Two Sequences Relation classification datasets """
+
+import torch
+from torch.utils.data import Dataset
+
+class TwoSeqClassDataset(Dataset):
+    """
+    Dataset class for two sequences relation classification datasets.
+    """
+
+    def __init__(self, data):
+        """
+
+        """
+        self.premises_lengths = [len(seq) for seq in data["premises"]]
+        self.max_premise_length = max_premise_length
+        if self.max_premise_length is None:
+            self.max_premise_length = max(self.premises_lengths)
+
+        self.hypotheses_lengths = [len(seq) for seq in data["hypotheses"]]
+        self.max_hypothesis_length = max_hypothesis_length
+        if self.max_hypothesis_length is None:
+            self.max_hypothesis_length = max(self.hypotheses_lengths)
+
+        self.num_sequences = len(data["premises"])
+
+        self.data = {"ids": [],
+                     "premises": torch.ones((self.num_sequences,
+                                             self.max_premise_length),
+                                            dtype=torch.long) * padding_idx,
+                     "hypotheses": torch.ones((self.num_sequences,
+                                               self.max_hypothesis_length),
+                                              dtype=torch.long) * padding_idx,
+                     "labels": torch.tensor(data["labels"], dtype=torch.long)}
+
+        for i, premise in enumerate(data["premises"]):
+            self.data["ids"].append(data["ids"][i])
+            end = min(len(premise), self.max_premise_length)
+            self.data["premises"][i][:end] = torch.tensor(premise[:end])
+
+            hypothesis = data["hypotheses"][i]
+            end = min(len(hypothesis), self.max_hypothesis_length)
+            self.data["hypotheses"][i][:end] = torch.tensor(hypothesis[:end])
+
+    def __len__(self):
+        return self.num_sequences
+
+    def __getitem__(self, index):
+        return {"id": self.data["ids"][index],
+                "premise": self.data["premises"][index],
+                "premise_length": min(self.premises_lengths[index],
+                                      self.max_premise_length),
+                "hypothesis": self.data["hypotheses"][index],
+                "hypothesis_length": min(self.hypotheses_lengths[index],
+                                         self.max_hypothesis_length),
+                "label": self.data["labels"][index]}
